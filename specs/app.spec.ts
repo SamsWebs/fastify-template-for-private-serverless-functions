05dc05e7ff92ac1f1d5d48379668bbba114e2e1f
Will Sams <will@samswebs.com>
2025-01-03 22:48:41 -0500

    Update test

---
+++ specs/app.spec.ts
@@ -0,0 +1,22 @@
+import supertest from 'supertest';
+import { fastify } from '../src/app';
+
+describe('GET /get/about', () => {
+    beforeAll(async () => {
+        await fastify.ready();
+    });
+
+    afterAll(async () => {
+        await fastify.close();
+    });
+
+    it('should return status Success and the ABOUT_MESSAGE', async () => {
+        process.env.ABOUT_MESSAGE = 'This is a test message';
+        const response = await supertest(fastify.server).get('/get/about');
+        expect(response.status).toBe(200);
+        expect(response.body).toEqual({
+            status: 'Success',
+            result: 'This is a test message',
+        });
+    });
+});
