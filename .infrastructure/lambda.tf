743f9722b32e028e22838d811fb1b75eae571973
Will Sams <will@samswebs.com>
2025-01-03 23:26:36 -0500

    Add infrastructure

---
+++ .infrastructure/lambda.tf
@@ -0,0 +1,20 @@
+resource "aws_lambda_function" "lambda_function" {
+  function_name = "${var.environment}-${var.function_name}-lambda_function"
+  timeout       = var.function_timeout
+  image_uri     = "${local.account_id}.dkr.ecr.${var.region}.amazonaws.com/${var.function_name}:latest"
+  package_type  = "Image"
+
+  role = aws_iam_role.lambda-function-role.arn
+
+  environment {
+    variables = {
+      ENVIRONMENT = var.environment
+      ABOUT_MESSAGE = "This message should not exist.  Did you not modify the template?"
+    }
+  }
+
+  depends_on = [
+    aws_iam_role_policy_attachment.lambda_logs,
+    aws_cloudwatch_log_group.cloudwatch-log-group,
+  ]
+}
