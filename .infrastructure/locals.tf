00e423fa3dcdf7b7baae6300440367b8c81fa5b0
Will Sams <will@samswebs.com>
2026-05-26 08:06:18 -0400

    Fix Node 22 parity, infrastructure bugs, and dockerignore typo
    - Align Dockerfile, CI, and .nvmrc on Node 22
    - Fix CloudWatch log group path (/lambda/ -> /aws/lambda/)
    - Fix prod log_retention_in_days 360 -> 365 (invalid CloudWatch value)
    - Remove unused default_vpc data source from locals.tf
    - Add Version field to IAM assume-role policy
    - Add required_providers and required_version to provider.tf
    - Fix .dockerignore typo (.infrastucture/ -> .infrastructure/)

--- .infrastructure/locals.tf
+++ .infrastructure/locals.tf
@@ -1,10 +1,5 @@
 data "aws_caller_identity" "current" {}
 
-data "aws_vpc" "default" {
-  default = true
-}
-
 locals {
-  account_id  = data.aws_caller_identity.current.account_id
-  default_vpc = data.aws_vpc.default
+  account_id = data.aws_caller_identity.current.account_id
 }
